a8a5a9cec6bb6b83052571df8031fa458625e976
pravesh-Blog <praveshsharma36429268310@gmail.com>
2026-08-13 14:03:00 +0530

    Google Analytics Added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +9,19 @@ export default function RootLayout({ children }) {
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
-
+        {/* google analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NJHG78H7HF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NJHG78H7HF');
+          `}
+        </Script>
         <Providers>
           {children}
         </Providers>
